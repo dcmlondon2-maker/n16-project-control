65f4a3e86438df4eb34694478c6b10eb46663045
dcmlondon2-maker <dcmlondon2@gmail.com>
2026-05-02 16:17:17 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-cimport React, ant cant{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 import { supabase } from "./supabaseClient";
 
